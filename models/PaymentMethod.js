73d0eb4644d3bc0a776a841c29af92fddb824c1d
Levy Koech <koechlevy07@gmail.com>
2026-08-07 10:16:40 +0300

    feat(account): add customer payment-methods CRUD and user coupons endpoints

---
+++ models/PaymentMethod.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const paymentMethodSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+    type: {
+        type: String,
+        enum: ['mpesa', 'visa', 'mastercard', 'paypal', 'bank_transfer'],
+        required: true
+    },
+    nickname: { type: String, trim: true, maxlength: 60 },
+    details: { type: String, trim: true, required: true, maxlength: 120 },
+    isDefault: { type: Boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model('PaymentMethod', paymentMethodSchema);
